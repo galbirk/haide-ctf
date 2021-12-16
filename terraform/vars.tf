392b0f7f594ad9458177665fe7f051fe73ea691d
Gal Birkman <galbirkman@gmail.com>
2021-12-16 16:33:39 +0200

    changes in tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,11 +1,3 @@
-# variable "appId" {
-#   description = "Azure Kubernetes Service Cluster service principal"
-# }
-
-# variable "password" {
-#   description = "Azure Kubernetes Service Cluster password"
-# }
-
 variable "rg-name" {
     description = "Azure Resource Group to Create the Cluster in"
     type = string
