0014ae56254b1fe6d057a2b00ed73c0b3fa0c2e5
Gal Birkman <galbirkman@gmail.com>
2021-12-23 12:25:48 +0200

    build

--- 2048/init.sh
+++ 2048/init.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
-exec 2>&1 /usr/sbin/sshd -D -e -p 2${SSH_PORT}
+
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+# Change Apache Port
+sed -i 's/\<VirtualHost \*\:80\>/\<VirtualHost \*\:80'"$APACHE_PORT"'\>/g' /etc/apache2/sites-enabled/000-default.conf
+
+
+echo "root:Aa123456" | chpasswd
 
 service apache2 start
+
+exec 2>&1 /usr/sbin/sshd -D -e -p 2${SSH_PORT}
